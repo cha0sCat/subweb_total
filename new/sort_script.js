5fbe8f74c85ba79f5fc6f73c6fd53e4279dffe35
Anonymous <>
2023-05-13 09:37:21 +0900

    update: 不再将 IEPL 特殊排序

--- new/sort_script.js
+++ new/sort_script.js
@@ -1,12 +1,12 @@
 function compare(node_a, node_b) {
     // 先按 香港、日本、其他、免费的顺序进行粗略排序
     // 再按照节点标号进行细分排序
-    if (node_a.Remark.includes("IEPL") && !node_b.Remark.includes("IEPL")) {
-        return true;
-    }
-    if (!node_a.Remark.includes("IEPL") && node_b.Remark.includes("IEPL")) {
-        return false;
-    }
+    // if (node_a.Remark.includes("IEPL") && !node_b.Remark.includes("IEPL")) {
+    //     return true;
+    // }
+    // if (!node_a.Remark.includes("IEPL") && node_b.Remark.includes("IEPL")) {
+    //     return false;
+    // }
     if (node_a.Remark.includes("港") && !node_b.Remark.includes("港")) {
         return true;
     }
